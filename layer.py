ae38db3791bc439c9ebad1fc4b35c03bd1826766
Andres-ramos <ramosandres443@gmail.com>
2024-09-29 13:54:19 -0400

    funciona. Needs cleanup

--- layer.py
+++ layer.py
@@ -4,18 +4,20 @@ from arcgis.gis import GIS, Item
 from pathlib import Path
 from arcgis.features import FeatureLayer, FeatureSet
 
-
+from arcgis import geometry
+from copy import deepcopy
+import geopandas as gpd 
+from arcgis.features import FeatureLayerCollection
 
 
 class Layer:
-    def __init__(self, gis, layer_title, arcgis_storage_folder):
+    def __init__(self, gis, layer_title, arcgis_storage_folder, color):
         self.gis = gis
         self.layer_title = layer_title
         self.file_path = self._generate_file_path()
         self.layer_item = self._get_layer_item(layer_title)
-        print(self.layer_item)
         self.arcgis_storage_folder = arcgis_storage_folder
-        self.layer_style = None
+        self.layer_style = self._create_style(color) if color else None
 
     def _generate_file_path(self):
         return f"./geojson_data/{self.layer_title}.geojson"
@@ -31,7 +33,7 @@ class Layer:
                 return p_layer_item
         return None
     
-    def update_or_create(self, wm, geojson):
+    def update_or_create(self, geojson):
 
         # If no layer with title name have been created, create layer and update map
         if self.layer_item == None:
@@ -54,8 +56,34 @@ class Layer:
             #Supone que no se remueven features del anterior
             # Se pasan solo las que se anadieron
             # 
-            layer_item = self._update_layer(geojson)
-            return "update"
+            if len(json.loads(geojson)["features"]) > 0:
+                if self.layer_title == "report_layer":
+
+                    # layer_item = self._update_layer(geojson)
+                    fe_collection = FeatureLayerCollection.fromitem(self.layer_item)
+                    with open(self.file_path, 'w') as f:
+                        f.write(geojson)
+
+                    fe_collection.manager.overwrite(self.file_path)
+                    return "update"
+
+                elif self.layer_title == "over_reservation_layer":
+
+                    fe_collection = FeatureLayerCollection.fromitem(self.layer_item)
+                    with open(self.file_path, 'w') as f:
+                        f.write(geojson)
+
+                    fe_collection.manager.overwrite(self.file_path)
+                    return "update"
+                elif self.layer_title == "non_overlap_reservation_layer":
+                  
+                    fe_collection = FeatureLayerCollection.fromitem(self.layer_item)
+                    with open(self.file_path, 'w') as f:
+                        f.write(geojson)
+
+                    fe_collection.manager.overwrite(self.file_path)
+
+                    return "update"
         return 
     
     def _create_layer(
@@ -112,13 +140,14 @@ class Layer:
                     "tags": "parcels",
                     "type": "GeoJson"
                 },
-                str(local_layer_file_path),
+                data=str(local_layer_file_path),
                 folder = storage_folder
             )
 
             return item.publish()
-        except Exception:
-            raise Exception("Arcgis layer upload exception")
+        except Exception as e:
+            print(e)
+            # raise Exception("Arcgis layer upload exception")
     
     #TODO: Geojson should only be new features
     def _update_layer(self, geojson: str) -> None:
@@ -127,10 +156,56 @@ class Layer:
         Updates a layer already in arcgis online
         """
         geojson_dict = json.loads(geojson)
+        updated_feature_list = []
         updated_features = FeatureSet.from_geojson(geojson_dict)
+        template = updated_features.features[0]
+        gdf = updated_features.sdf
+        for _, row in gdf.iterrows():
+            new_feature = deepcopy(template)
+            print(new_feature)
+            input_geometry = {'y':float(row['SHAPE']['y']),
+                       'x':float(row['SHAPE']['x'])}
+            output_geometry = geometry.project(geometries = [input_geometry],
+                                    in_sr = 6566, 
+                                    out_sr = 6566)
+            new_feature.geometry = output_geometry[0]
+            new_feature.attributes['¿Qué ves? Tipo de observación'] = row['¿Qué ves? Tipo de observación']
+            new_feature.attributes["globalid"] = row["globalid"]
+            new_feature.attributes["Pueblo"] = row["Pueblo"]
+            new_feature.attributes["¿Hay otras observaciones aledañas al lugar?"] = row["¿Hay otras observaciones aledañas al lugar?"]
+            new_feature.attributes["¿Qué otras observaciones viste?"] = row["¿Qué otras observaciones viste?"]
+            print(new_feature.attributes)
+            updated_feature_list.append(new_feature)
+        print("hasya aqui")
         try :
             f = FeatureLayer.fromitem(self.layer_item)
-            f.edit_features(adds=updated_features)
-            return f
-        except Exception:
+            f.edit_features(adds=updated_feature_list)
+        except Exception as e:
+            print(e)
             raise Exception("Arcgis update error")
+
+    def _create_style(self,color):
+        outline_color = [0, 0, 0, 255]
+        outline_width = 0.75
+        color_map = {
+            "red": [255, 0, 0, 100],
+            "green": [0,255,0,100],
+            "blue": [0,0,255,100]
+        }
+        rgba_color = color_map[color]
+
+        return {"renderer": {
+            'type': 'simple', 
+            'symbol': {
+                'type': 'esriSFS', 
+                'style': 'esriSFSSolid', 
+                'color': rgba_color, 
+                'outline': {
+                    'type': 'esriSLS', 
+                    'style': 'esriSLSSolid', 
+                    'color': outline_color, 
+                    'width': outline_width
+                    }
+                }
+            }
+        }
--- layer_factory/factory.py
+++ layer_factory/factory.py
@@ -1,19 +1,33 @@
 import geopandas as gpd
 
-#TODO: Figure out how to optimize non overlap layer 
-#TODO: Figure out where to put the buffer_size 
 class LayerFactory:
     def __init__(self):
-        self.RESERVAS_TERRESTRES_FOLDER = "./reservas_terrestres"
+        self.RESERVAS_TERRESTRES_FOLDER = "./reservas_marinas"
 
     def generate_layer(self, layer_name, gdf):
         if layer_name == "report":
             # TODO: Clean column names
-            # gdf = gdf.drop(columns=['globalid', 'CreationDa', 'Creator', 'EditDate', 'Editor',
-            #     'si_es_una_', 'venta_cont', 'a_ade_come', 'nombre', 'correo_ele',
-            #     'telefono', 'esta_const', 'este_terre', 'si_de_casu', 'pueblo'
-            #     ])
-            print(len(gdf))
+            gdf = gdf.drop(columns=['CreationDa', 'Creator', 'EditDate', 'Editor',
+                'tel_fono', 'nombre_y_a', 'correo_ele', 'categoria', 'si_es_cons',
+                'pregunta_s', '_se_puede_', 'si_es_una_', 'si_es_una1', 'si_es_un_1', 
+                'si_es_un_2', '_tuvo_prob', 'si_tienes_', 'a_ade_come', 'comentario', 
+                'field_21_o', '_hay_algun'])
+            gdf = gdf.rename(columns={
+                "_qu_ves_ti": "¿Qué ves? Tipo de observación", 
+                "pueblo_en_": "Pueblo",
+                "_hay_otras": "¿Hay otras observaciones aledañas al lugar?",
+                "_se_pudo_o": "¿Qué otras observaciones viste?"
+                })
+            # column_map = {
+            #     "": "¿Hay otras observaciones aledañas al lugar?",
+            #     "": "¿Qué otras observaciones viste?",
+            #     "": "¿Hay alguna otra observación en el mismo lugar?",
+            #     "": "¿Se puede observar más de una situación de la observada anteriormente?",
+            #     "": "Añade comentarios, características o mayor descripción."
+            #     "": "Comentarios generales"
+            # }
+            # gdf['test_col'] = gdf['_qu_ves_ti'].apply(lambda row: row[0:5])
+
             return gdf.to_json()
         
         elif layer_name == "overlap":
@@ -24,6 +38,15 @@ class LayerFactory:
             circle_gdf["geometry"] = circle_gdf["geometry"].to_crs(epsg=6566)
             reserve_gdf = gpd.read_file(self.RESERVAS_TERRESTRES_FOLDER)
             intersecting_gdf = gpd.sjoin(reserve_gdf, circle_gdf, how='inner', op='intersects')
+            print(intersecting_gdf)
+            intersecting_gdf = intersecting_gdf.drop(columns=[ 'loc_desig','terr_mar', 'gis_source', 'notes', 
+            'index_right'])
+            intersecting_gdf = intersecting_gdf.rename(columns={
+                "names": "Nombre",
+                "mgmt": "Manejador",
+                "ownership": "Dueño",
+                "year_estab": "Año establecida"
+            })
             return intersecting_gdf.to_json()
 
         elif layer_name == "non_overlap":
@@ -35,4 +58,16 @@ class LayerFactory:
             reserve_gdf = gpd.read_file(self.RESERVAS_TERRESTRES_FOLDER)
             intersecting_gdf = gpd.sjoin(reserve_gdf, circle_gdf, how='inner', op='intersects')
             non_intersecting_gdf = reserve_gdf[~reserve_gdf.index.isin(intersecting_gdf.index)]
+            print(non_intersecting_gdf.columns)
+            non_intersecting_gdf = non_intersecting_gdf.drop(columns=[ 'loc_desig','terr_mar', 'gis_source', 'notes'])
+            non_intersecting_gdf = non_intersecting_gdf.rename(columns={
+                "names": "Nombre",
+                "mgmt": "Manejador",
+                "ownership": "Dueño",
+                "year_estab": "Año establecida"
+            })
             return non_intersecting_gdf.to_json()
+    
+
+    def _foo(gdf):
+        return 
