3f41ba9aeaac7f2246a2215cec0308893e97e729
Andres-ramos <ramosandres443@gmail.com>
2024-10-06 12:54:18 -0400

    Added temp dir for survey data

--- etl/extract.py
+++ etl/extract.py
@@ -1,21 +1,22 @@
 from arcgis.gis import GIS, ContentManager
 from arcgis.apps.survey123 import SurveyManager
 import geopandas as gpd 
+import tempfile
 
 def download_survey_data(
         survey_id:str,
         content_manager:ContentManager, 
         survey_manager:SurveyManager, 
-        download_path='survey_data'
     ) -> str:
     """
     Receives survey manager, content manager and survey_id
     downloads survey data into download path
     Returns - download path
     """
+    temp_dir = tempfile.TemporaryDirectory().name
     survey_item = content_manager.get(survey_id)
     survey_obj = survey_manager.get(survey_item.id)
-    path = survey_obj.download(export_format="Shapefile", save_folder=download_path)
+    path = survey_obj.download(export_format="Shapefile", save_folder=temp_dir)
     return path
 
 
